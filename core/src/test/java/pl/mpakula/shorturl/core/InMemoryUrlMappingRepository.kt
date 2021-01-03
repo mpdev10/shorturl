11b8a48a3af70caed54b82cd16cb31bd0538ac2c
mateuszp <mpftp10@gmail.com>
2021-01-03 17:34:24 +0100

    Removed unnecessary dependencies

--- core/src/test/java/pl/mpakula/shorturl/core/InMemoryUrlMappingRepository.kt
+++ core/src/test/java/pl/mpakula/shorturl/core/InMemoryUrlMappingRepository.kt
@@ -1,13 +1,13 @@
 package pl.mpakula.shorturl.core
 
-import com.google.common.collect.Maps
 import pl.mpakula.shorturl.core.model.UrlMapping
 import pl.mpakula.shorturl.core.ports.outgoing.UrlMappingRepository
 import java.math.BigInteger
+import java.util.concurrent.ConcurrentHashMap
 
 internal class InMemoryUrlMappingRepository : UrlMappingRepository {
 
-    private val shortenedUrls: MutableMap<BigInteger, UrlMapping> = Maps.newConcurrentMap()
+    private val shortenedUrls: MutableMap<BigInteger, UrlMapping> = ConcurrentHashMap()
 
     override fun save(urlMapping: UrlMapping): UrlMapping {
         val id = urlMapping.retrieveId()
